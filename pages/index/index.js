9d37cc2ac60cb90f9821b8fdb84ff4f8079a3a43
luyandan <2103029766@qq.com>
2022-11-10 23:15:00 +0800

    完成翻译功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+import { translate } from "../../utils/api";
 const app = getApp();
 
 Page({
@@ -5,6 +6,7 @@ Page({
     query: "",
     curLang: {},
     hideClearIcon: true,
+    result: [],
   },
   onLoad: function (options) {
     console.log("onLoad");
@@ -26,5 +28,23 @@ Page({
   },
   onTapClose: function () {
     this.setData({ query: "", hideClearIcon: true });
+  },
+  onConfirm: function () {
+    if (!this.data.query) return;
+    translate(this.data.query, {
+      from: "auto",
+      to: this.data.curLang.lang,
+    }).then((res) => {
+      // console.log("res", res);
+      this.setData({ result: res.trans_result });
+      console.log("result", this.data.result);
+      let history = wx.getStorageSync("history") || [];
+      history.unshift({
+        query: this.data.query,
+        result: res.trans_result[0].dst,
+      });
+      history.length = history.length > 10 ? 10 : history.length;
+      wx.setStorageSync("history", history);
+    });
   },
 });
