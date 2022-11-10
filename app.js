b50af12ec93526744e41e32c7193ad43c6a3bec9
luyandan <2103029766@qq.com>
2022-11-10 16:18:29 +0800

    update

--- app.js
+++ app.js
@@ -2,18 +2,59 @@
 App({
   onLaunch() {
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    this.globalData.curLang =
+      wx.getStorageSync("curLang") || this.globalData.langList[0];
+
+    // const logs = wx.getStorageSync("logs") || [];
+    // logs.unshift(Date.now());
+    // wx.setStorageSync("logs", logs);
+
+    // // 登录
+    // wx.login({
+    //   success: (res) => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   },
+    // });
   },
   globalData: {
-    userInfo: null
-  }
-})
+    curLang: {},
+    langList: [
+      {
+        chs: "英文",
+        lang: "en",
+        index: 0,
+      },
+      {
+        chs: "中文",
+        lang: "zh",
+        index: 1,
+      },
+      {
+        chs: "日语",
+        lang: "jp",
+        index: 2,
+      },
+      {
+        chs: "韩语",
+        lang: "kor",
+        index: 3,
+      },
+      {
+        chs: "法语",
+        lang: "fra",
+        index: 4,
+      },
+      {
+        chs: "西班牙语",
+        lang: "spa",
+        index: 5,
+      },
+      {
+        chs: "阿拉伯语",
+        lang: "ara",
+        index: 6,
+      },
+    ],
+  },
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,30 @@
 const app = getApp();
 
 Page({
-  data: {},
+  data: {
+    query: "",
+    curLang: {},
+    hideClearIcon: true,
+  },
+  onLoad: function (options) {
+    console.log("onLoad");
+  },
+  onShow: function () {
+    console.log("onShow", app.globalData);
+    if (this.data.curLang.lang !== app.globalData.curLang.lang) {
+      this.setData({ curLang: app.globalData.curLang });
+      //  this.onConfirm()
+    }
+  },
+  onInput: function (e) {
+    this.setData({ query: e.detail.value });
+    if (this.data.query.length > 0) {
+      this.setData({ hideClearIcon: false });
+    } else {
+      this.setData({ hideClearIcon: true });
+    }
+  },
+  onTapClose: function () {
+    this.setData({ query: "", hideClearIcon: true });
+  },
 });
