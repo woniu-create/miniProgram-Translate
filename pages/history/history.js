55db568d349c0633a4de3fc690c10a04c1884539
luyandan <2103029766@qq.com>
2022-11-14 14:59:33 +0800

    历史页面及功能

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,5 +1,16 @@
 const app = getApp();
 
 Page({
-  data: {},
+  data: {
+    history: [],
+  },
+  onShow: function () {
+    this.setData({ history: wx.getStorageSync("history") });
+    console.log("history", this.data.history);
+  },
+  onTapItem: function (e) {
+    wx.reLaunch({
+      url: `/pages/index/index?query=${e.currentTarget.dataset.query}`,
+    });
+  },
 });
